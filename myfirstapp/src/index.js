1030e31fcc1e83bbc41940113f75d431879d86c7
abhishek081999 <abhishekbhoranj@gmail.com>
2024-03-12 23:15:07 +0530

    diffine vikash tag with file name vikash

--- myfirstapp/src/index.js
+++ myfirstapp/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Vikash from './vikash';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Vikash />
   </React.StrictMode>
 );
 
---
+++ myfirstapp/src/vikash.js
@@ -0,0 +1,6 @@
+function Vikash(){
+    return ( <h1>chl chutiye apna kam kar </h1>
+
+
+);};
+export default Vikash
